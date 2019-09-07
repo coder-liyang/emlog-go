99f91a72514de5d709a0dca0187cb66ec6cb7828
liyang <liyangcoder@163.com>
2019-09-07 20:37:52 +0800

    页面支持layout

--- controllers/default.go
+++ controllers/default.go
@@ -12,40 +12,49 @@ type MainController struct {
 	beego.Controller
 }
 
-//首页
-func (c *MainController) Index() {
-	var (
-		page int64
-	)
+func setOptions(c *MainController) {
 	//配置项
 	options, err := models.GetAllOptions()
 	if err != nil {
 		c.Ctx.WriteString(err.Error())
 	}
+	for _, item := range options {
+		c.Data[item.OptionName] = item.OptionValue
+	}
+}
+
+//首页
+func (c *MainController) Index() {
+	var (
+		page int64
+	)
+	setOptions(c)
+
 	//文章列表
 	//pageStr := c.GetString("p")
 	//page, _ = strconv.ParseInt(pageStr, 10, 64)
 	page, _ = c.GetInt64("p")
-	blogs, err := models.GetLogsForHome(page)
-	for _, item := range options {
-		c.Data[item.OptionName] = item.OptionValue
-	}
+	blogs, _ := models.GetLogsForHome(page)
 	c.Data["blogs"] = &blogs
 	//分页数据
-	count, err := models.BlogTotal()
+	count, _ := models.BlogTotal()
 	p := pagination.NewPaginator(c.Ctx.Request, 10, count)
 	c.Data["paginator"] = p
 
+	c.Layout = "layouts/front.tpl"
 	c.TplName = "index.tpl"
 }
 
 //文章详情页
 func (c *MainController) Content() {
+	setOptions(c)
 	gid, _ := strconv.ParseInt(c.Ctx.Input.Param(":gid"), 10, 64)
 	blog, err := models.GetBlogByGid(gid)
 	if err != nil {
 		fmt.Println(gid, err.Error())
 	}
-	c.Data["blog"] = blog
+	c.Data["blog"] = &blog
+
+	c.Layout = "layouts/front.tpl"
 	c.TplName = "single.tpl"
 }
